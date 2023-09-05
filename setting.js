d7d40667feab996099177534249822aa1a94ab2b
Jack Hsiao <chhsiao@gms.tcu.edu.tw>
2023-09-06 03:26:04 +0800

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,4 +1,4 @@
 ﻿// var FHIRrootURL = "http://140.110.13.115:8080/r5/fhir";
-var FHIRrootURL ="http://hapi.fhir.org/baseR4/";
+var FHIRrootURL ="https://hapi.fhir.org/baseR4/";
 
 // var FHIRrootURL = "https://hapi.fhir.org/baseDstu3/"; 
